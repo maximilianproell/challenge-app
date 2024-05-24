15a3be9eea487ad14cf304cb8873d0cf29d7b6b1
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2024-05-24 15:39:55 +0200

    - Improved text styling and position on QrCodeScannerScreen

--- leQuestShared/src/androidMain/kotlin/ui/scanner/QrCodeScannerScreen.kt
+++ leQuestShared/src/androidMain/kotlin/ui/scanner/QrCodeScannerScreen.kt
@@ -6,14 +6,19 @@ import androidx.camera.mlkit.vision.MlKitAnalyzer
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
@@ -113,6 +118,16 @@ fun QrCodeScannerScreen(
             }
         )
     } else {
-        Text(stringResource(Res.string.allow_camera_permission))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = stringResource(Res.string.allow_camera_permission),
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
